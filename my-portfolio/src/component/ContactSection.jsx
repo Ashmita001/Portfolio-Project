ead056cad9142e03e1d797449761887ba0b8a018
Ashmita001 <ashmita.202004@gmail.com>
2025-03-12 21:06:57 +0530

    Contact page done

--- my-portfolio/src/component/ContactSection.jsx
+++ my-portfolio/src/component/ContactSection.jsx
@@ -1,74 +1,40 @@
-import React from 'react'
-import mail from '../assets/email.png'
-import linkedin from '../assets/linkedin.png'
-import github from '../assets/github.png'
-import discord from '../assets/discord.png'
-import instagram from '../assets/insta.png'
-import twitter from '../assets/twitterx.png'
+import React from 'react';
+import ActionButton from './ActionButton';
+import mail from '../assets/email.png';
+import linkedin from '../assets/linkedin.png';
+import github from '../assets/github.png';
+import discord from '../assets/discord.png';
+import instagram from '../assets/insta.png';
+import twitter from '../assets/twitterx.png';
 
 const ContactSection = () => {
-    const openLink = (url) => {
-        window.open(url, '_blank', 'noopener,noreferrer');
-      }
     return (
         <>
-            <div className='flex flex-col px-8'>
-                <div className='py-50'></div>
-                <p className='text-center text-[32px] md:text-[32px] lg:text-[32px] font-normal py-2'>Let's Connect</p>
-                <p className='text-center text-[16px] md:text-[16px] lg:text-[16px] font-normal py-2'>Have a project in mind or just want to say hello? Let&apos;s create something amazing together!</p>
+            <div className='place-content-center h-[90vh]'>
+                <div className='flex flex-col px-8'>
+                    <div className=''></div>
+                    <p className='text-center text-[32px] font-normal py-2'>Let's Connect</p>
+                    <p className='text-center text-[13px] md:text-[16px] font-normal pb-8 md:px-40'>
+                        Have a project in mind or just want to say hello? Let&apos;s create something amazing together!
+                    </p>
+                </div>
 
-
-            </div>
-
-            <div className=' grid justify-items-center grid-cols-6 md:grid-cols-6 md:gap-0 gap-0 md:px-48 px-8 pb-8'>
-
-                <button className='bg-[#D86072] border-2 justify-items-center [box-shadow:4px_4px_0px_black] w-[3em] h-[3em] p-1
-                 hover:translate-2 duration-300 ease-in-out transform hover:scale-105 hover:[box-shadow:0px_0px_0px_black] focus:border-[#000000] focus:outline-none active:[box-shadow:0px_0px_0px_black]  active:scale-95 
-                 ' onClick={() => openLink('mailto:ashmita.202004@gmail.com')}>
-                    <img src={mail} className='' alt="mail" />
-                </button>
-                <button className='bg-[#D6DC82] border-2 justify-items-center [box-shadow:4px_4px_0px_black] w-[3em] h-[3em] p-1
-                hover:translate-2 duration-300 ease-in-out transform hover:scale-105 hover:[box-shadow:0px_0px_0px_black] focus:border-[#000000] focus:outline-none active:scale-95 active:[box-shadow:0px_0px_0px_black]  '
-                onClick={() => openLink('https://github.com/Ashmita001')}>
-                    <img src={github} className='' alt="github" />
-                </button>
-                <button className='bg-[#c6c2f2] border-2 justify-items-center [box-shadow:4px_4px_0px_black] w-[3em] h-[3em] p-1 
-                hover:translate-2 duration-300 ease-in-out transform hover:scale-105 hover:[box-shadow:0px_0px_0px_black] focus:border-[#000000] focus:outline-none active:scale-95 active:[box-shadow:0px_0px_0px_black] '
-                onClick={() => openLink('https://www.linkedin.com/in/ashmita-328350273/')}>
-                    <img src={linkedin} className='' alt="linkedin" />
-                </button>
-                <button className='bg-[#c6c2f2] border-2 justify-items-center [box-shadow:4px_4px_0px_black] w-[3em] h-[3em] p-1
-                hover:translate-2 duration-300 ease-in-out transform hover:scale-105 hover:[box-shadow:0px_0px_0px_black] focus:border-[#000000] focus:outline-none active:scale-95 active:[box-shadow:0px_0px_0px_black] '
-                onClick={() => openLink('http://discordapp.com/users/ashmita.01')}
-                
-                >
-                    <img src={discord} className='' alt="discord" />
-                </button>
-                <button className='bg-[#D6DC82] border-2 justify-items-center [box-shadow:4px_4px_0px_black] w-[3em] h-[3em] p-1
-                hover:translate-2 duration-300 ease-in-out transform hover:scale-105 hover:[box-shadow:0px_0px_0px_black] focus:border-[#000000] focus:outline-none active:scale-95 active:[box-shadow:0px_0px_0px_black] '
-                onClick={() => openLink('https://www.instagram.com/yupp_its_ash/')}>
-                
-                    <img src={instagram} className='' alt="instagram" />
-                </button>
-                <button className='bg-[#D86072] border-2 justify-items-center [box-shadow:4px_4px_0px_black] w-[3em] h-[3em] p-1
-                hover:translate-2 duration-300 ease-in-out transform hover:scale-105 hover:[box-shadow:0px_0px_0px_black] focus:border-[#000000] focus:outline-none active:scale-95 active:[box-shadow:0px_0px_0px_black] '
-                onClick={() => openLink('https://twitter.com/its_ashmita21')}>
-                    <img src={twitter} className='' alt="twitter" />
-                </button>
-
-
+                <div className='grid justify-items-center grid-cols-6 md:grid-cols-6 md:gap-0 gap-0 md:px-48 lg:px-68 px-8 pb-8'>
+                    <ActionButton url="mailto:ashmita.202004@gmail.com" icon={mail} alt="mail" color="#D86072" />
+                    <ActionButton url="https://github.com/Ashmita001" icon={github} alt="github" color="#D6DC82" />
+                    <ActionButton url="https://www.linkedin.com/in/ashmita-328350273/" icon={linkedin} alt="linkedin" color="#c6c2f2" />
+                    <ActionButton url="http://discordapp.com/users/ashmita.01" icon={discord} alt="discord" color="#D86072" />
+                    <ActionButton url="https://www.instagram.com/yupp_its_ash/" icon={instagram} alt="instagram" color="#D6DC82" />
+                    <ActionButton url="https://twitter.com/its_ashmita21" icon={twitter} alt="twitter" color="#c6c2f2" />
+                </div>
             </div>
 
 
+            <div>
+                <p className='text-[#d86072] [text-shadow:1px_0px_0px_black,-1px_0px_0px_black,0px_1px_0px_black,0px_-1px_0px_black,1px_1px_0px_black,-1px_-1px_0px_black,1px_-1px_0px_black,-1px_1px_0px_black] font-normal tracking-wide text-center text-xl px-16 lg:text-2xl pb-8'>Thank You for <span className='text-[#D6DC82]'>watching</span> my <span className='text-[#c6c2f2]'>portfolio</span></p>
+            </div>
         </>
+    );
+};
 
-
-
-
-
-
-
-    )
-}
-
-export default ContactSection
+export default ContactSection;
